5ab8decbcdb667b0fa63f81f654b0ec7e97ba757
severjason <ssever2010@gmail.com>
2023-01-06 13:12:18 +0200

    update gtag strategy

--- src/shared/analytics/gtag.tsx
+++ src/shared/analytics/gtag.tsx
@@ -5,10 +5,10 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 const Gtag = () => {
   return GA_TRACKING_ID ? (
     <>
-      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
       <Script
         id="gtag-init"
-        strategy="lazyOnload"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
