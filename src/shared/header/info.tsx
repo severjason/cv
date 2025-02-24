252fb4c140fc7ec9a5fa06f5c91e8fd16b591ee7
severjason <ssever2010@gmail.com>
2025-02-24 22:47:08 +0300

    minor packages update

--- src/shared/header/info.tsx
+++ src/shared/header/info.tsx
@@ -51,9 +51,7 @@ const Info = () => {
               className={getBtnClx({ variant: 'link' })}
               onClick={handleClick}
             >
-              <span className={'flex print:hidden'}>
-                {emailIsShown ? data?.main_info?.email : t('common:links.email')}
-              </span>
+              <span className={'flex'}>{emailIsShown ? data?.main_info?.email : t('common:links.email')}</span>
               <span className="hidden hover:flex">{data?.main_info?.email}</span>
             </Link>
           }
